d7d18a48d33e3e6c8e19a67efb4556f564b15c21
olga saneeva <olgasaneeva@mail.ru>
2021-08-18 15:36:40 +0300

    add 6

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,4 +20,14 @@ def create
     render :new, status: :unprocessable_entity
   end
 end
+def destroy
+  @article = Article.find(params[:id])
+  @article.destroy
+
+  redirect_to root_path
+end
+    private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
 end
